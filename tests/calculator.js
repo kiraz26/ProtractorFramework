d7d98c5b6b3cbe0c923359cc16f47c34b61a5cf4
kiraz26 <halilkirazz@gmail.com>
2020-03-29 15:17:15 -0700

    my first commit

---
+++ tests/calculator.js
@@ -0,0 +1,31 @@
+let homepage = require("../pages/homapage");
+
+describe("demo calculator tests", function() {
+  it("addition test", function() {
+    homepage.get("http://juliemr.github.io/protractor-demo/");
+
+    homepage.enterFirstNumber("4");
+
+    homepage.enterSecondNumber("3");
+
+    homepage.clickGo();
+
+    homepage.verifyResult("7");
+
+    browser.sleep(3000);
+  });
+
+  it("substraction test", function() {
+    homepage.get("http://juliemr.github.io/protractor-demo/");
+
+    homepage.enterFirstNumber("4");
+
+    homepage.enterSecondNumber("3");
+
+    homepage.clickGo1();
+
+    homepage.verifyResult("7");
+
+    browser.sleep(3000);
+  });
+});
